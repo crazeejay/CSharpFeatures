e37c5efef3938e693b5c1136c35f78f6f35139a8
jay le cornu <crazeejay@live.com.au>
2017-08-15 16:11:08 +1000

    Added Arrays Topic

---
+++ Assets/5-Arrays/Scripts/Turret.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Arrays
+{
+    public class Turret : MonoBehaviour
+    {
+        public Transform target;
+
+        private Weapon currentWeapon;
+
+        // Use this for initialization
+        void Start()
+        {
+            currentWeapon = GetComponentInChildren<Weapon>();
+            currentWeapon.SetTarget(target);
+        }
+
+    }
+}
---
+++ Assets/5-Arrays/Scripts/Weapon.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Arrays
+{
+    public class Weapon : MonoBehaviour
+    {
+        public int damage = 10;
+        public int maxBullets = 30;
+        public float bulletSpeed = 20f;
+        public float fireInterval = 0.2f;
+        public GameObject bulletPrefab;
+        public Transform spawnPoint;
+
+        private Bullet[] spawnedBullets;
+        private int currentBullets = 0;
+        private bool isFired = false;
+        private Transform target;
+
+        // Use this for initialization
+        void Start()
+        {
+            spawnedBullets = new Bullet[maxBullets];
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            //IF !isFired AND currentBullets < maxBullets
+            if(!isFired && currentBullets < maxBullets)
+            {
+                // StartCoroutine Fire()
+                StartCoroutine(Fire());
+            }
+        }
+
+        IEnumerator Fire()
+        {
+            //Run whatever is here first
+            isFired = true;
+
+            //Spawn the bullet
+            SpawnBullet();
+
+            yield return new WaitForSeconds(fireInterval); //wait a few seconds
+
+            //Run whatever is here last
+            isFired = false;
+        }
+
+        void SpawnBullet()
+        {
+            //1. Instatiate a bullet clone
+            GameObject clone = Instantiate(bulletPrefab, spawnPoint.position, Quaternion.identity);
+            //2. Make a direction that goes to target
+            Vector2 direction = target.position - transform.position;
+            direction.Normalize();
+            //3. Rotate barrel
+            Vector3 eulerAngles = transform.eulerAngles;
+
+            float angle = Vector3.Angle(Vector3.right, direction);
+            eulerAngles.z = angle;
+
+            transform.eulerAngles = eulerAngles;
+            //4. Grab bullet script from clone
+            Bullet bullet = clone.GetComponent<Bullet>();
+            //5. Send bullet to target
+            bullet.direction = direction;
+            //6. Store bullet in array
+            spawnedBullets[currentBullets] = bullet;
+            //7. Increment currentBullets
+            currentBullets++;
+        }
+
+        public void SetTarget(Transform target)
+        {
+            this.target = target;
+        }
+    }
+}
