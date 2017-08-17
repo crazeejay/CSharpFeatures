015d900c006e5ab7862841014b302a431c26dcca
crazeejay <crazeejay@live.com.au>
2017-08-17 11:56:31 +1000

    Added Collisions Topic & Billiards

---
+++ Assets/6-Collisions/Scripts/SpaceStation.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Collision
+{
+    public class SpaceStation : MonoBehaviour
+    {
+        public float rotationSpeed = 20f;
+
+        // Update is called once per frame
+        void Update()
+        {
+            transform.Rotate(Vector3.forward, rotationSpeed * Time.deltaTime);
+        }
+    }
+}
---
+++ Assets/~Billiards/Scripts/Ball.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Billiards
+{
+    [RequireComponent(typeof(Rigidbody))]
+    public class Ball : MonoBehaviour
+    {
+        public float gravity = -9.81f;
+
+        private Rigidbody rigid;
+
+        // Use this for initialization
+        void Start()
+        {
+            rigid = GetComponent<Rigidbody>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            rigid.velocity = rigid.velocity.normalized + Vector3.back * gravity;
+        }
+    }
+}
