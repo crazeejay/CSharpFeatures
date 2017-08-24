8d22926323c4c8b07a4d909325fac1515bcf9cf9
crazeejay <crazeejay@live.com.au>
2017-08-24 12:09:46 +1000

    made proper billiards

--- Assets/~Billiards/Scripts/Ball.cs
+++ Assets/~Billiards/Scripts/Ball.cs
@@ -7,7 +7,7 @@ namespace Billiards
     [RequireComponent(typeof(Rigidbody))]
     public class Ball : MonoBehaviour
     {
-        public float gravity = -9.81f;
+        public float stopSpeed = 0.2f;
 
         private Rigidbody rigid;
 
@@ -17,10 +17,32 @@ namespace Billiards
             rigid = GetComponent<Rigidbody>();
         }
 
-        // Update is called once per frame
-        void Update()
+        void FixedUpdate()
         {
-            rigid.velocity = rigid.velocity.normalized + Vector3.back * gravity;
+            Vector3 vel = rigid.velocity;
+
+            //If velocity.y > 0
+            if (vel.y > 0)
+            {
+                //Cap velocity at zero
+                vel.y = 0;
+            }
+
+            //If velocity.magnitude < stopSpeed
+            if(vel.magnitude < stopSpeed)
+            {
+                //Cancel out the velocity
+                vel = Vector3.zero;
+            }
+
+            //Apply desired 'vel' to rigid's velocity
+            rigid.velocity = vel;
+
+        }
+
+        public void Hit(Vector3 direction, float impactForce)
+        {
+            rigid.AddForce(direction * impactForce, ForceMode.Impulse);
         }
     }
 }
--- Assets/~Billiards/Scripts/Cue.cs
+++ Assets/~Billiards/Scripts/Cue.cs
@@ -2,15 +2,97 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Cue : MonoBehaviour {
+namespace Billiards
+{
+    public class Cue : MonoBehaviour
+    {
+        public Ball targetBall;
+        public float minPower = 0f;
+        public float maxPower = 20f;
+        public float maxDistance = 5f;
+        //slider element here (CHALLENGE ACCEPTED)
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+        private float hitPower;
+        private Vector3 aimDirection;
+        private Vector3 prevMousePos;
+        private Ray mouseRay;
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.DrawLine(mouseRay.origin, mouseRay.origin + mouseRay.direction * 1000f);
+            Gizmos.color = Color.red;
+            Gizmos.DrawLine(targetBall.transform.position, targetBall.transform.position + aimDirection * hitPower);
+        }
+
+        void Aim()
+        {
+            mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            //out = Treat the argument as 'output' and fill the variable with data
+            if(Physics.Raycast(mouseRay, out hit))
+            {
+                Vector3 dir = transform.position - hit.point;
+                float angle = Mathf.Atan2(dir.x, dir.z) * Mathf.Rad2Deg;
+                transform.rotation = Quaternion.AngleAxis(angle, Vector3.up);
+                transform.position = targetBall.transform.position;
+            }
+        }
+        public void Activate()
+        {
+            Aim();
+            gameObject.SetActive(true);
+        }
+
+        void Deactivate()
+        {
+            gameObject.SetActive(false);
+        }
+
+        void Drag()
+        {
+            Vector3 targetPos = targetBall.transform.position;
+            Vector3 currMousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            float distance = Vector3.Distance(prevMousePos, currMousePos);
+            distance = Mathf.Clamp(distance, 0, maxDistance);
+            //maxDistance = 20
+            //distance = 5
+            //percentage = distance / maxDistance
+            //percentage = 5 / 20
+            //percentage = .25
+            float distPercentage = distance / maxDistance;
+            hitPower = Mathf.Lerp(minPower, maxPower, distPercentage);
+
+            transform.position = targetPos - transform.forward * distance;
+            aimDirection = (targetPos - transform.position).normalized;
+        }
+
+        void Fire()
+        {
+            targetBall.Hit(aimDirection, hitPower);
+            Deactivate();
+        }
+        
+        // Update is called once per frame
+        void Update()
+        {
+            if(Input.GetMouseButtonDown(0))
+            {
+                prevMousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            }
+
+            if(Input.GetMouseButton(0))
+            {
+                Drag();
+            }
+            else
+            {
+                Aim();
+            }
+
+            if(Input.GetMouseButtonUp(0))
+            {
+                Fire();
+            }
+        }
+    }
 }
