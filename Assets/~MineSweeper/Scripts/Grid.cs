2f57c0e2b2482f759749b33e95b42ff89ee861de
crazeejay <crazeejay@live.com.au>
2017-08-29 15:50:32 +1000

    Added Mine Sweeper and Improved breakout

---
+++ Assets/~MineSweeper/Scripts/Grid.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Minesweeper2D
+{
+
+    public class Grid : MonoBehaviour
+    {
+        public GameObject tilePrefab;
+        public int width = 10;
+        public int height = 10;
+        public float spacing = .155f;
+
+        private Tile[,] tiles;
+
+        // Use this for initialization
+        void Start()
+        {
+            //Generate tiles on startup
+            GenerateTiles();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        //functionality for spawning tiles
+        Tile SpawnTile(Vector3 pos)
+        {
+            //Clone tile prefab
+            GameObject clone = Instantiate(tilePrefab);
+            clone.transform.position = pos; // position tile
+            Tile currentTile = clone.GetComponent<Tile>(); //Get Tile component
+            return currentTile; //return it
+       }
+
+        //spawns tiles  in a grid-like pattern
+        void GenerateTiles()
+        {
+            //creates new 2D array of size width x height
+            tiles = new Tile[width, height];
+
+            //Loop through the entire tile list
+            for (int x = 0; x < width; x++)
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    //store halfsource for later
+                    Vector2 halfsize = new Vector2(width / 2, height / 2);
+                    //pivot tiles around grid
+                    Vector2 pos = new Vector2(x - halfsize.x, 
+                                              y - halfsize.y);
+                    //Apply spacing
+                    pos *= spacing;
+
+                    //spawn tile
+                    Tile tile = SpawnTile(pos);
+                    //Attach newly spawned tile
+                    tile.transform.SetParent(transform);
+                    //store it's array coordinates within itself  for future reference 
+                    tile.x = x;
+                    tile.y = y;
+                    //Store tile in array at those coordinates
+                    tiles[x, y] = tile;
+                }
+            }
+        }
+
+    }
+    
+}
---
+++ Assets/~MineSweeper/Scripts/Tile.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Minesweeper2D
+{
+    [RequireComponent(typeof(SpriteRenderer))]
+    public class Tile : MonoBehaviour
+    {
+
+        //store x & y coordinate in array for later use
+        public int x = 0;
+        public int y = 0;
+        public bool isMine = false; //is the current tile a mine?
+        public bool isRevealed = false; //had the tile already been revealed?
+        [Header("References")]
+        public Sprite[] emptySprites; //List of empty sprites e.g 1,2,3,4 ... etc
+        public Sprite[] mineSprites; // the mine sprites
+
+        private SpriteRenderer rend; //Reference to sprite renderer
+
+
+        void Awake()
+        {
+            //Grab reference to sprite renderer
+            rend = GetComponent<SpriteRenderer>();
+        }
+ 
+
+        // Use this for initialization
+        void Start()
+        {
+            //Randomly decide if its a mine or not
+            isMine = Random.value < 0.5f;
+        }
+
+        public void Reveal(int adjacentMines, int mineState = 0)
+        {
+            //Flages the tile as being revealed
+            isRevealed = true;
+            //Check if tile is a mine
+            if(isMine)
+            {
+                //Sets sprite to mine sprite
+                rend.sprite = emptySprites[mineState];
+            }
+            else
+            {
+                //sets sprite to appropriate texture based on adjecent mines
+                rend.sprite = emptySprites[adjacentMines];
+            }
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
