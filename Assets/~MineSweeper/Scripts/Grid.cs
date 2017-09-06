7c99fd24fc32cf2654153e934270277e9b22126d
jay le cornu <crazeejay@live.com.au>
2017-09-06 21:53:11 +1000

    Fixed MineSweeper Code at 9:53pm

--- Assets/~MineSweeper/Scripts/Grid.cs
+++ Assets/~MineSweeper/Scripts/Grid.cs
@@ -1,4 +1,4 @@
-﻿/*using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -30,41 +30,38 @@ namespace Minesweeper2D
         //functionality for spawning tiles
         Tile SpawnTile(Vector3 pos)
         {
-           //Clone tile prefab
+            // Clone tile prefab
             GameObject clone = Instantiate(tilePrefab);
-            clone.transform.position = 
-            // position tile
-            Tile currentTile = clone.GetComponent<Tile>(); //Get Tile component
-            return currentTile; //return it
-       }
-       
-        //spawns tiles  in a grid-like pattern
+            clone.transform.position = pos; // Position tile
+            Tile currentTile = clone.GetComponent<Tile>(); // Get tile component
+            return currentTile; // Return it
+        }
+
+        // Spawns tiles in a grid-like pattern
         void GenerateTiles()
         {
-            //creates new 2D array of size width x height
+            // Create new 2D array of size width x height
             tiles = new Tile[width, height];
 
-            //Loop through the entire tile list
+            // Loop through the entire tile list
             for (int x = 0; x < width; x++)
             {
                 for (int y = 0; y < height; y++)
                 {
-                    //store halfsource for later
-                    Vector2 halfsize = new Vector2(width / 2, height / 2);
-                    //pivot tiles around grid
-                    Vector2 pos = new Vector2(x - halfsize.x, 
-                                              y - halfsize.y);
-                    //Apply spacing
+                    // Store half size for later use
+                    Vector2 halfSize = new Vector2(width / 2, height / 2);
+                    // Pivot tiles around Grid
+                    Vector2 pos = new Vector2(x - halfSize.x + offset, y - halfSize.y + offset);
+                    // Apply spacing
                     pos *= spacing;
-
-                    //spawn tile
+                    // Spawn the tile
                     Tile tile = SpawnTile(pos);
-                    //Attach newly spawned tile
+                    // Attach newly spawned tile to
                     tile.transform.SetParent(transform);
-                    //store it's array coordinates within itself  for future reference 
+                    // Store it's array coordinates within itself for future reference
                     tile.x = x;
                     tile.y = y;
-                    //Store tile in array at those coordinates
+                    // Store tile in array at those coordinates
                     tiles[x, y] = tile;
                 }
             }
@@ -83,6 +80,5 @@ namespace Minesweeper2D
         }
 
     }
-    
+
 }
-*/
