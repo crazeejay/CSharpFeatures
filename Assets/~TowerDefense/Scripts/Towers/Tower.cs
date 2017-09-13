f5cb2e7136a3f7720b582599801605ba0e57b718
jay le cornu <crazeejay@live.com.au>
2017-09-13 22:24:57 +1000

    made more changes to tower defense code still having issues

--- Assets/~TowerDefense/Scripts/Towers/Tower.cs
+++ Assets/~TowerDefense/Scripts/Towers/Tower.cs
@@ -2,15 +2,38 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Tower : MonoBehaviour {
+namespace TowerDefense
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public class Tower : MonoBehaviour
+    {
+        public Cannon cannon;
+        public float attackRate = 0.25f;
+        public float attackRadius = 5f;
+        private float attackTimer = 0f;
+        private List<Enemy> enemies = new List<Enemy>();
+
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        void OnTriggerEnter(Collider col)
+        {
+            if (col.gameObject.tag == "Enemy")
+            {
+                Destroy(col.gameObject);
+
+                Destroy(gameObject);
+            }
+        }
+    }
 }
