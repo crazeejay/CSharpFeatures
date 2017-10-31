c71a42f0c8a92b5539653b8719d395ada6c56636
crazeejay <crazeejay@live.com.au>
2017-10-31 16:14:53 +1100

    Asteroids Added

---
+++ Assets/~Asteroids/Scripts/Moving.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Asteroids
+{
+    public class Moving : MonoBehaviour
+    {
+        public float acceleration = 50f;
+        public float rotationSpeed = 360f;
+
+        private Rigidbody2D rigid;
+
+        // Use this for initialization
+        void Start()
+        {
+            rigid = GetComponent<Rigidbody2D>();
+        }
+
+        void Accelerate()
+        {
+            float inputV = Input.GetAxis("Vertical");
+            rigid.AddForce(transform.up * inputV * acceleration);
+        }
+
+        void Rotate()
+        {
+            float inputH = Input.GetAxis("Vertical");
+            transform.Rotate(Vector3.back, rotationSpeed * inputH * Time.deltaTime);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            Accelerate();
+            Rotate();
+
+        }
+    }
+}
---
+++ Assets/~Asteroids/Scripts/Shooting.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Asteroids
+{
+    public class Shooting : MonoBehaviour
+    {
+        public GameObject bulletPrefab;
+        public float bulletSpeed = 20f;
+        public float shootRate = 0.2f;
+
+        private float shootTimer = 0f;
+
+        //Fire a bullet
+        void Shoot()
+        {
+            //Create a new bullet clone
+            GameObject clone = Instantiate(bulletPrefab, transform.position, transform.rotation);
+            //Grab the Rigidbody2D from the clone
+            Rigidbody2D rigid = clone.GetComponent<Rigidbody2D>();
+            //Add a force to the bullet (using speed)s
+            rigid.AddForce(transform.up * bulletSpeed, ForceMode2D.Impulse);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            // Count up shootTimer (in seconds)
+            shootTimer += Time.deltaTime;
+            //IF shootTimer > shootRate
+            if(shootTimer > shootRate)
+            {
+                //IF Space bar is pressed
+                if (Input.GetKeyDown(KeyCode.Space))
+                {
+                    //Shoot a projectile
+                    Shoot();
+                    //Reset shootTimer
+                    shootTimer = 0f;
+                }
+            }
+            
+        }
+    }
+}
